76d8efa3dfb4e4ac11f4758b7fed248cf15e3132
KarineAlsa <saremana@hotmail.com>
2024-06-25 10:32:43 -0600

    url

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const PORT = 9000;
 app.get('/event',VerifyToken,getAllEvents);
 app.get('/event/:id',VerifyToken,getByIdEvent);
 
-app.use('/event', proxy('http://18.209.250.86:1234'));
+app.use('/event', proxy('http://104.248.49.221:1234'));
 app.use('/user',proxy('http://146.190.64.233:3000'))
 
 app.listen(PORT, () => {
