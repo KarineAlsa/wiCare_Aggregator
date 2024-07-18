39dcc303aec378d06a9d29213c3b1a652be4c8fc
KarineAlsa <saremana@hotmail.com>
2024-07-18 13:37:38 -0600

    dionation

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,7 @@ const PORT = 9000;
 
 app.use('/event', proxy('http://104.248.49.221:1234'));
 app.use('/user',proxy('http://146.190.64.233:3000'))
+app.use('/donation',proxy('http://142.93.14.203:4321'))
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
